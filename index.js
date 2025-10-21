fe64983716f08b3e5ed42e6fe2e279d15261b410
Hritik Kumar <144543568+hritik095@users.noreply.github.com>
2025-10-21 17:45:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -59,3 +59,6 @@ app.listen(PORT, async () => {
   await connectDB();
   console.log(`Server is running on port ${PORT}`);
 });
+app.get('/', (req,res)=>{
+      res.send("DailyBasket server is running");
+});
